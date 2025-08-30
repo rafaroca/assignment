21ed0214f8a31af6c75b8154804d8621ba48eac2
Raffael Gerlach <raffael.gerlach@codecentric.de>
2025-08-30 19:50:02 +0200

    rename otelcommon import

--- otlp-log-processor-backend-go/logs_service_test.go
+++ otlp-log-processor-backend-go/logs_service_test.go
@@ -3,103 +3,103 @@ package main
 import (
 	"testing"
 
-	v1 "go.opentelemetry.io/proto/otlp/common/v1"
+	otelcommon "go.opentelemetry.io/proto/otlp/common/v1"
 )
 
 func TestExtractStringValue(t *testing.T) {
 	tests := map[string]struct {
-		input    *v1.AnyValue
+		input    *otelcommon.AnyValue
 		expected string
 	}{
 		"StringValue": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_StringValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_StringValue{
 					StringValue: "test string",
 				},
 			},
 			expected: "test string",
 		},
 		"BoolValue_True": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_BoolValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_BoolValue{
 					BoolValue: true,
 				},
 			},
 			expected: "true",
 		},
 		"BoolValue_False": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_BoolValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_BoolValue{
 					BoolValue: false,
 				},
 			},
 			expected: "false",
 		},
 		"IntValue_Positive": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_IntValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_IntValue{
 					IntValue: 42,
 				},
 			},
 			expected: "42",
 		},
 		"IntValue_Negative": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_IntValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_IntValue{
 					IntValue: -123,
 				},
 			},
 			expected: "-123",
 		},
 		"IntValue_Zero": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_IntValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_IntValue{
 					IntValue: 0,
 				},
 			},
 			expected: "0",
 		},
 		"DoubleValue_Positive": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_DoubleValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_DoubleValue{
 					DoubleValue: 3.14159,
 				},
 			},
 			expected: "3.141590",
 		},
 		"DoubleValue_Negative": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_DoubleValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_DoubleValue{
 					DoubleValue: -2.718,
 				},
 			},
 			expected: "-2.718000",
 		},
 		"DoubleValue_Zero": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_DoubleValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_DoubleValue{
 					DoubleValue: 0.0,
 				},
 			},
 			expected: "0.000000",
 		},
 		"ArrayValue_Empty": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_ArrayValue{
-					ArrayValue: &v1.ArrayValue{
-						Values: []*v1.AnyValue{},
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_ArrayValue{
+					ArrayValue: &otelcommon.ArrayValue{
+						Values: []*otelcommon.AnyValue{},
 					},
 				},
 			},
 			expected: "[]",
 		},
 		"ArrayValue_SingleString": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_ArrayValue{
-					ArrayValue: &v1.ArrayValue{
-						Values: []*v1.AnyValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_ArrayValue{
+					ArrayValue: &otelcommon.ArrayValue{
+						Values: []*otelcommon.AnyValue{
 							{
-								Value: &v1.AnyValue_StringValue{
+								Value: &otelcommon.AnyValue_StringValue{
 									StringValue: "item1",
 								},
 							},
@@ -110,22 +110,22 @@ func TestExtractStringValue(t *testing.T) {
 			expected: "[item1]",
 		},
 		"ArrayValue_Multiple": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_ArrayValue{
-					ArrayValue: &v1.ArrayValue{
-						Values: []*v1.AnyValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_ArrayValue{
+					ArrayValue: &otelcommon.ArrayValue{
+						Values: []*otelcommon.AnyValue{
 							{
-								Value: &v1.AnyValue_StringValue{
+								Value: &otelcommon.AnyValue_StringValue{
 									StringValue: "item1",
 								},
 							},
 							{
-								Value: &v1.AnyValue_IntValue{
+								Value: &otelcommon.AnyValue_IntValue{
 									IntValue: 42,
 								},
 							},
 							{
-								Value: &v1.AnyValue_BoolValue{
+								Value: &otelcommon.AnyValue_BoolValue{
 									BoolValue: true,
 								},
 							},
@@ -136,24 +136,24 @@ func TestExtractStringValue(t *testing.T) {
 			expected: "[item1,42,true]",
 		},
 		"KvlistValue_Empty": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_KvlistValue{
-					KvlistValue: &v1.KeyValueList{
-						Values: []*v1.KeyValue{},
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_KvlistValue{
+					KvlistValue: &otelcommon.KeyValueList{
+						Values: []*otelcommon.KeyValue{},
 					},
 				},
 			},
 			expected: "{}",
 		},
 		"KvlistValue_SinglePair": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_KvlistValue{
-					KvlistValue: &v1.KeyValueList{
-						Values: []*v1.KeyValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_KvlistValue{
+					KvlistValue: &otelcommon.KeyValueList{
+						Values: []*otelcommon.KeyValue{
 							{
 								Key: "key1",
-								Value: &v1.AnyValue{
-									Value: &v1.AnyValue_StringValue{
+								Value: &otelcommon.AnyValue{
+									Value: &otelcommon.AnyValue_StringValue{
 										StringValue: "value1",
 									},
 								},
@@ -165,22 +165,22 @@ func TestExtractStringValue(t *testing.T) {
 			expected: "{key1:value1}",
 		},
 		"KvlistValue_MultiplePairs": {
-			input: &v1.AnyValue{
-				Value: &v1.AnyValue_KvlistValue{
-					KvlistValue: &v1.KeyValueList{
-						Values: []*v1.KeyValue{
+			input: &otelcommon.AnyValue{
+				Value: &otelcommon.AnyValue_KvlistValue{
+					KvlistValue: &otelcommon.KeyValueList{
+						Values: []*otelcommon.KeyValue{
 							{
 								Key: "key1",
-								Value: &v1.AnyValue{
-									Value: &v1.AnyValue_StringValue{
+								Value: &otelcommon.AnyValue{
+									Value: &otelcommon.AnyValue_StringValue{
 										StringValue: "value1",
 									},
 								},
 							},
 							{
 								Key: "key2",
-								Value: &v1.AnyValue{
-									Value: &v1.AnyValue_IntValue{
+								Value: &otelcommon.AnyValue{
+									Value: &otelcommon.AnyValue_IntValue{
 										IntValue: 123,
 									},
 								},
@@ -192,13 +192,13 @@ func TestExtractStringValue(t *testing.T) {
 			expected: "{key1:value1,key2:123}",
 		},
 		"NilValue": {
-			input: &v1.AnyValue{
+			input: &otelcommon.AnyValue{
 				Value: nil,
 			},
 			expected: "",
 		},
 		"NilAnyValue": {
-			input: nil,
+			input:    nil,
 			expected: "",
 		},
 	}
